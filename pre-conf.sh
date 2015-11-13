db90be29cdb0f75ac58cbe515f88443a401ed980
Christian Boulanger <info@bibliograph.org>
2015-11-13 17:36:44 +0100

    Install Humhub cron jobs

--- pre-conf.sh
+++ pre-conf.sh
@@ -14,8 +14,8 @@ killall mysqld
 echo "Database initialized ..."
 sleep 5s
 
-# Install crontabs
+# Install cron jobs
 (crontab -l ; echo "*/5 * * * * su -c \"/var/www/humhub/protected/yii  cron/hourly\" www-data    >/dev/null 2>&1") | crontab -
 (crontab -l ; echo "0 18 * * * su -c \"/var/www/humhub/protected/yii  cron/daily\" www-data >/dev/null 2>&1") | crontab -
-echo "Installed cron jobs ..."
+echo "Installed cron jobs ..." 
 
