66c98429fe0ea2919d03485897e71ecf833a2efa
Christian Boulanger <info@bibliograph.org>
2015-11-13 17:34:25 +0100

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Initiatize mysql database
 /usr/bin/mysqld_safe &
 sleep 5s
 
@@ -10,4 +11,11 @@ mysqladmin -u root -pHuMhUb create humhub
 echo "GRANT ALL ON humhub.* TO humhub@localhost IDENTIFIED BY 'HuMhUb'; flush privileges; " | mysql -u root -pHuMhUb
 
 killall mysqld
+echo "Database initialized ..."
 sleep 5s
+
+# Install crontabs
+(crontab -l ; echo "*/5 * * * * su -c \"/var/www/humhub/protected/yii  cron/hourly\" www-data    >/dev/null 2>&1") | crontab -
+(crontab -l ; echo "0 18 * * * su -c \"/var/www/humhub/protected/yii  cron/daily\" www-data >/dev/null 2>&1") | crontab -
+echo "Installed cron jobs ..."
+
